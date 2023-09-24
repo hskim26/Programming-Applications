1e7bb8e5e537a7cc2067ec84f2d92a6c57ffefd2
hskim26 <132140134+hskim26@users.noreply.github.com>
2023-09-24 13:51:59 +0900

    Add files via upload

---
+++ static.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void sub() {
+	static int scount = 0;
+	int acount = 0;
+	printf("scount = %d\t", scount);
+	printf("acount = %d\n", acount);
+	scount++;
+	acount++;
+}
+
+
+int main() {
+
+	sub();
+	sub();
+	sub();
+
+
+	return 0;
+}
